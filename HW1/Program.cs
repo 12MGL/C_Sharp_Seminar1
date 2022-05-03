a94dc8cef1887b58024c8e533c65073debf64f6a
BARS <makz.ars@gmail.com>
2022-05-03 20:47:04 +0300

    максимальное из трёх чисел

---
+++ HW1/Program.cs
@@ -0,0 +1,24 @@
+﻿//программа, принимающая 3 числа и выдающее максимальное из них
+Console.WriteLine("введите число 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 3");
+int numberC = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("максимальное число равно ");
+if (numberA > numberB)
+{
+    if (numberA > numberC)
+    {
+        Console.WriteLine(numberA);
+    }
+    else
+        Console.WriteLine(numberC);
+}
+else 
+    if (numberB > numberC)
+    {
+        Console.WriteLine(numberB);
+    }
+    else
+        Console.WriteLine(numberC);
