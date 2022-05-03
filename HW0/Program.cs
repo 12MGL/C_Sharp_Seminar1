6e20d209cc20d05ec684c10cab96cbe5379d992f
BARS <makz.ars@gmail.com>
2022-05-03 20:39:54 +0300

    домашняя задача про большее-меньшее число из двух

---
+++ HW0/Program.cs
@@ -0,0 +1,16 @@
+﻿//программа, принимающая 2 числа, и выдающая, какое из них большее, какое - меньшее
+Console.WriteLine("введите число 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA < numberB)
+{
+    Console.WriteLine("второе число больше");
+}
+else
+if (numberA > numberB)
+{
+    Console.WriteLine("первое число больше");
+}
+else 
+Console.WriteLine("числа равны");
