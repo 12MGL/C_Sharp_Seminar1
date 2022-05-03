fc04fabbb86a8c8255e3d3c2ff0ba3e4f5294889
BARS <makz.ars@gmail.com>
2022-05-03 20:27:34 +0300

    модуль числа

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿//вычисление модуля числа
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("модуль введённого числа равен");
+if (number < 0)
+{
+    number = -number;
+
+}
+Console.WriteLine(number);
