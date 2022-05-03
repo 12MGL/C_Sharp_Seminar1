be2b385103577cbe2dde866f123ee0f29e2d0e52
BARS <makz.ars@gmail.com>
2022-05-03 21:26:41 +0300

    домашняя задача про чётность числа

---
+++ HW2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, определяющая, чётное ли число
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int ostatok = number%2;
+if (ostatok == 1)
+{
+    Console.WriteLine("число нечётное");
+}
+else 
+Console.WriteLine("число чётное");
