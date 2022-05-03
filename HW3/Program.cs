2417f59f1868c385896f943c68ae740738cab41d
BARS <makz.ars@gmail.com>
2022-05-03 21:48:49 +0300

    задача про вывод целых чисел от 1 до введённого

---
+++ HW3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача, выдающая все чётные числа от 1 до введённого
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    Console.Write("введите число больше 1");
+}
+else
+{
+    if (number == 0)
+    {
+        Console.WriteLine("число 0 - чётное");
+    }
+    else
+    {
+        if (number == 1)
+        {
+            Console.Write("Введите число больше 1");
+        }
+        else
+        {
+            Console.WriteLine("Чётные числа от 1 до введённого: ");
+            for (int count = 2; count < number+1; count++)
+            {
+                if (count%2==0)
+                {
+                    Console.Write(count);
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
