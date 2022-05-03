2eae38b09a6ebeb56cdc1d7e314af2a1cc139d40
BARS <makz.ars@gmail.com>
2022-05-03 20:33:42 +0300

    задача про обратное число

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿ //задача, которое принимает на вход одно значение, а на выходе выводит обратное ( 4 -- 0.25, 8 -- 0.125)
+ Console.WriteLine("введите число");
+ double number = Convert.ToInt32(Console.ReadLine());
+ if (number == 0)
+ {
+     Console.WriteLine("на ноль делить нельзя");
+ }
+ else
+ {
+ double obr = 1 / number;
+ Console.Write("обратное число равно ");
+ Console.WriteLine(obr);
+ }
