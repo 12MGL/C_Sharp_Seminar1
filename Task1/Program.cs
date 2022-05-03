9093c1a4a18034e2a7e338720b38daa1776a5b7f
BARS <makz.ars@gmail.com>
2022-05-03 19:48:52 +0300

    задача 1 - про квадрат числа

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Написать программу, проверяющую, является ли первое число квадратом второго
+Console.WriteLine("введите число-квадрат");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int realsquare = number*number;
+if (square == realsquare)
+{
+    Console.WriteLine("правильно");
+}
+else
+{
+    Console.WriteLine("неправильно");
+}
