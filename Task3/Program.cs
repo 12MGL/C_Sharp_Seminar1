252671369ad9346631fff512834d95bd1ba28912
BARS <makz.ars@gmail.com>
2022-05-03 20:06:54 +0300

    зачада про квадрат числа

---
+++ Task3/Program.cs
@@ -0,0 +1,5 @@
+﻿//программа возведения числа в квадрат
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("квадрат равен ");
+Console.WriteLine(number*number);
